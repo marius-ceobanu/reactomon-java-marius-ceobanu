9dbd84fddc89c1c61a1c1da1b521a5f5a9571043
Marius Ceobanu <ceobanu.marius@gmail.com>
2020-11-27 16:31:17 +0200

    Refactor App as functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 import Header from "./components/layout/Header";
@@ -7,38 +7,37 @@ import PokemonDetails from "./components/PokemonDetails";
 import TypeList from "./components/TypeList";
 import './App.css';
 
-class App extends Component {
-    state = {
-        pokemonCards: [],
-        types: []
-    }
+function App() {
 
-    componentDidMount() {
+    const [pokemonCards, setPokemonCards] = useState([]);
+    const [types, setTypes] = useState([]);
+
+    useEffect(() => {
+        console.log("didMount");
         axios.get('https://pokeapi.co/api/v2/pokemon')
-            .then(res => this.setState({ pokemonCards: res.data.results }));
+                .then(res => setPokemonCards(res.data.results ));
         axios.get('https://pokeapi.co/api/v2/type')
-            .then(res => this.setState({ types: res.data.results }))
-    }
+            .then(res => setTypes(res.data.results ));
+    }, []);
 
-    render() {
-        return (
-            <Router>
-                <div className="App" style={pageStyle}>
-                    <div className="container">
-                        <Header />
-                        <Route exact path={["/", "/pokemons"]}>
-                            <PokemonList pokemonCards={this.state.pokemonCards} />
-                        </Route>
-                        <Route exact path="/types">
-                            <TypeList types={this.state.types} />
-                        </Route>
-                        <Route path="/pokemon/:id" component={PokemonDetails} />
-                    </div>
+    return (
+        <Router>
+            <div className="App" style={pageStyle}>
+                <div className="container">
+                    <Header />
+                    <Route exact path={["/", "/pokemons"]}>
+                        <PokemonList pokemonCards={pokemonCards} />
+                    </Route>
+                    <Route exact path="/types">
+                        <TypeList types={types} />
+                    </Route>
+                    <Route path="/pokemon/:id" component={PokemonDetails} />
                 </div>
-            </Router>
-        );
-    }
+            </div>
+        </Router>
+    );
 }
+
 
 const pageStyle = {
     backgroundColor: '#2F4F4F',
