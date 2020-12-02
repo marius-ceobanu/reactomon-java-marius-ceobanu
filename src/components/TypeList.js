4eab4b2674e632e1980165cc11a4df4da7efe736
Marius Ceobanu <ceobanu.marius@gmail.com>
2020-12-02 13:22:53 +0200

    Small refactoring

--- src/components/TypeList.js
+++ src/components/TypeList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 import CardColumns from 'react-bootstrap/CardColumns';
 import TypeCard from "./TypeCard";
