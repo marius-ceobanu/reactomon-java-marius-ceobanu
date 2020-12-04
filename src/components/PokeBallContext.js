ad6fb9f22a56330a2d7e055269b694dc5926e5fe
Marius Ceobanu <ceobanu.marius@gmail.com>
2020-12-04 17:01:32 +0200

    Implement catch pokemons in localStorage for deployed version stability

--- src/components/PokeBallContext.js
+++ src/components/PokeBallContext.js
@@ -1,9 +1,13 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
 
 export const PokeBallContext = createContext();
 
 export const PokeBallProvider = (props) => {
-  const [pokeBall, catchPokemon] = useState([]);
+  const [pokeBall, catchPokemon] = useState(localStorage.getItem('myPokemons') ? JSON.parse(localStorage.getItem('myPokemons')) : []);
+
+  useEffect(() => {
+      localStorage.setItem('myPokemons', JSON.stringify(pokeBall));
+  }, [pokeBall]);
 
   return (
       <PokeBallContext.Provider value={[pokeBall, catchPokemon]} >
